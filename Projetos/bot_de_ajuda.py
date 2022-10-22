b185915173698e4e3ff2c405060fc4555f404127
Guilherme Ramos Ferreira <104803024+GuilhermeRamosFerreira@users.noreply.github.com>
2022-10-22 20:24:11 -0300

    Add files via upload

---
+++ Projetos/bot_de_ajuda.py
@@ -0,0 +1,29 @@
+print(
+		"Boa noite, seja bem vindo ai atendimento da VOCÊ TELECOM\n",
+	   	"Olá!\n",
+	   	"Sou a MEG, sua atendente visual\n",
+	   	"Em que posso te ajudar\n",
+	   	"1 - Vendas\n",
+	   	"2 - Suporte Técnico\n",
+	   	"3 - Segunda via de fatura\n",
+	   	"4 - Questões finaceiras\n",
+	   	"5 - Alteração no nome da rede e senha do wifi\n",
+	   	"6 - Serviços(mudar de endereço, mudar de plano e outros\n",
+	   	"7 - Cancelamento\n",
+	   	"8 - Informe de Pagamento\n"
+	 	)
+numero = int(input("Digite um dos numéros das opçoês acima: "))
+if numero == 1 :
+	print('1')
+elif numero == 2:
+	print('2')
+elif numero == 3:
+	print('3')
+elif numero == 4:
+	print('4')
+elif numero == 5:
+	print('5')
+elif numero == 6:
+	print('6')
+elif numero == 7:
+	print('7')
